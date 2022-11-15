082e51e1adc21e07764af981671a45caedec819d
1995yuta <githubkondoyuta@gmail.com>
2022-11-16 02:48:54 +0900

    エンコード・デコード処理の追加

--- js/main.js
+++ js/main.js
@@ -1,6 +1,24 @@
-function clickBtn() {
-    var textArea = document.getElementById('textArea').value;
-    textArea = textArea.split('\n');
-    var textArray = textArea.join('<br>');
-    $('#test').html(textArray);
-}
+$(function(){
+    $('.wrapper').on('click', '#changeBtn', function(){
+
+        $('.changeUrlText').remove();
+
+        var textArea = document.getElementById('textArea').value;
+        textArea = textArea.split('\n');
+
+        $.each(textArea, function(i, val) {
+            if (val.match(/q=/)) {
+                val = val.split('q=');
+                var sjisUrl = UnescapeUTF8(val[1]);
+                $('.sjisUrl').html(sjisUrl);
+                // Shift_JISコードのタイプで escape エンコードされた escapedString を元の文字列にデコードした値を返します。
+                var originText = UnescapeSJIS(sjisUrl);
+                $('.origin').html(originText);
+                // 文字列 -> UTF8にエンコード
+                var utf8Url = EscapeUTF8(originText);
+                val = val[0] + utf8Url;
+            }
+            $('#test').append('<p class="changeUrlText">' + val  + '</p>');
+        });
+    });
+});
